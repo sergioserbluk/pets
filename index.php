1efac0dc9cc9acfe556cad10cf0e15a91774b987
salapc1 <salapc1@redisiv.com>
2024-04-10 21:31:25 -0300

    cree el index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>welcome</h1>
+    <div class="barra">
+        <div class="logo"></div>
+        <a href="#">fotos</a>
+    </div>
+    <div class="contenedorPadre">
+        <div class="conthijo1"></div>
+        <div class="conthijo2"></div>
+    </div>
+</body>
+</html>
